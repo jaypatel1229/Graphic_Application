c378c8d113f3cd66160cc2fd782d250b5a361d95
jay.patel <jay.patel@1R-80>
2022-02-09 17:34:37 +0530

    Add project files.

---
+++ Graphic_App/CanvasDemo.cs
@@ -0,0 +1,57 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Graphic_App
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
+    class CanvasDemo : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            View v = new CanDemoView(this);
+            SetContentView(v);
+        }
+
+    }
+
+    public class CanDemoView : View
+    {
+        public CanDemoView(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnDraw(Canvas canvas)
+        {
+            base.OnDraw(canvas);
+
+            var paintBV = new Paint();
+            paintBV.Color = Color.BlueViolet;
+            paintBV.SetStyle(Paint.Style.FillAndStroke);
+
+            var paintY = new Paint();
+            paintY.Color = Color.Yellow;
+            paintY.SetStyle(Paint.Style.FillAndStroke);
+
+            float middle = canvas.Width * 0.50f;
+
+            canvas.DrawPaint(paintBV);
+
+            canvas.DrawRect(0, 0, middle, 500, paintY);
+
+
+
+
+        }
+    }
+}
---
+++ Graphic_App/MainActivity.cs
@@ -0,0 +1,59 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace Graphic_App
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Button myVectorDemo;
+        Button myDrawabledemo;
+        Button myCanvasDemo;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+
+            myVectorDemo = FindViewById<Button>(Resource.Id.button1);
+            myDrawabledemo = FindViewById<Button>(Resource.Id.button2);
+            myCanvasDemo = FindViewById<Button>(Resource.Id.button3);
+
+
+            myVectorDemo.Click += MyVectorDemo_Click;
+            myDrawabledemo.Click += MyDrawabledemo_Click;
+            myCanvasDemo.Click += MyCanvasDemo_Click;
+        }
+
+        private void MyCanvasDemo_Click(object sender, System.EventArgs e)
+        {
+            Intent k = new Intent(Application.Context, typeof(CanvasDemo));
+            StartActivity(k);
+        }
+
+        private void MyDrawabledemo_Click(object sender, System.EventArgs e)
+        {
+            Intent j = new Intent(Application.Context, typeof(DrawableDemo));
+            StartActivity(j);
+        }
+
+        private void MyVectorDemo_Click(object sender, System.EventArgs e)
+        {
+            Intent i = new Intent(Application.Context, typeof(VectorDemo));
+            StartActivity(i);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
